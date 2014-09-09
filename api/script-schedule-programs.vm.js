83d907fa95d54e79356bdd6936c6dba59991e379
Yuki MIZUNO <mizuyuu0904@gmail.com>
2014-09-10 06:44:59 +0900

    support access from xbmc-addon-iptvsimple

--- api/script-schedule-programs.vm.js
+++ api/script-schedule-programs.vm.js
@@ -1,7 +1,7 @@
 (function() {
 	
-	switch (request.method) {
-		case 'GET':
+	switch (request.type) {
+		case 'json':
 			var programs = [];
 			
 			data.schedule.forEach(function(ch) {
@@ -10,6 +10,37 @@
 			
 			response.head(200);
 			response.end(JSON.stringify(programs, null, '  '));
+			return;
+		case 'xml':
+			var xmlData = "";
+			xmlData += '<?xml version="1.0" encoding="UTF-8"?>\n'
+			xmlData += '<!DOCTYPE tv SYSTEM "xmltv.dtd">\n\n'
+			xmlData += '<tv generator-info-name="tsEPG2xml" generator-info-url="http://192.168.100.60:10772/api/schedule/programs.xml">\n'
+			
+			data.schedule.forEach(function(ch) {
+				xmlData += '  <channel id="' + ch.id + '" tp="' + ch.channel + '">\n'
+				xmlData += '    <display-name lang="ja_JP">' + ch.name + '</display-name>\n'
+				xmlData += '    <service_id>' + ch.sid + '</service_id>\n'
+				xmlData += '  </channel>\n'
+				ch.programs.forEach(function(prog) {
+					var s = new Date(prog.start)
+					var e = new Date(prog.end)
+					var startDate = "" + s.getFullYear() + ("0" + (s.getMonth() + 1)).slice(-2) + ("0" + s.getDate()).slice(-2) +
+						("0" + s.getHours()).slice(-2) + ("0" + s.getMinutes()).slice(-2) + ("0" + s.getSeconds()).slice(-2);
+					var endDate = "" + e.getFullYear() + ("0" + (e.getMonth() + 1)).slice(-2) + ("0" + e.getDate()).slice(-2) +
+						("0" + e.getHours()).slice(-2) + ("0" + e.getMinutes()).slice(-2) + ("0" + e.getSeconds()).slice(-2);
+					xmlData += '  <programme start="' + startDate + ' +0900" stop="' + endDate + ' +0900" channel="' + prog.channel.id + '" event_id="' + prog.id + '">\n'
+					xmlData += '    <title lang="ja_JP">' + prog.fullTitle.replace(/</g,'＜').replace(/>/g,'＞').replace(/&/g, '＆') + '</title>\n'
+					xmlData += '    <desc lang="ja_JP">' + prog.detail.replace(/</g,'＜').replace(/>/g,'＞').replace(/&/g, '＆') + '</desc>\n'
+					xmlData += '    <category lang="ja_JP">' + prog.category + '</category>\n'
+					xmlData += '    <category lang="en">' + prog.category + '</category>\n'
+					xmlData += '  </programme>\n'
+				});
+			});
+			xmlData += '</tv>'
+			
+			response.head(200);
+			response.end(xmlData);
 			return;
 	}
 
